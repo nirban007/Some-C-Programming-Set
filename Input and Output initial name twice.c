bb9e413d4b0ebf86ff83031dbe9d0f88d8ca628b
Nirban Mitra <57198481+nirban007@users.noreply.github.com>
2020-01-08 23:40:29 +0600

    Add files via upload

---
+++ Input and Output initial name twice.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char in[30];
+
+    scanf("%c", &in[30]);
+    printf("%c\n", in[30]);
+    scanf("%c", &in[30]);
+    printf("%c\n", in[30]);
+
+
+    return 0;
+
+}
