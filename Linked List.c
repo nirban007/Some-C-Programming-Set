fb552b62d14d70de989399a883ad3a2d3db21019
Nirban Mitra <57198481+nirban007@users.noreply.github.com>
2020-02-11 21:56:50 +0600

    Update Linked List.c

--- Linked List.c
+++ Linked List.c
@@ -1,3 +1,4 @@
+
 #include<stdio.h>
 
 int main()
@@ -25,19 +26,19 @@ int menu()
     }
     else if(op==2)
     {
-        printf("===Delete Operation===\n");
+        delete();
     }
     else if(op==3)
     {
-        printf("===Update Operation===\n");
+        update();
     }
     else if(op==4)
     {
-        printf("===Search Operation===\n");
+        search();
     }
     else if(op==5)
     {
-        printf("===Operation Closed===\n");
+        close();
         return op;
     }
     else
@@ -87,4 +88,80 @@ int insert()
 
 
 }
+int delete()
+{
+    int op;
+    printf("Select Insert List Operation\n");
+    printf("1. Delete\n");
+    printf("Input operation: ");
+    scanf("%d", &op);
+    if(op==1)
+    {
+        printf("Pop\n");
+
+    }
+    else
+    {
+        printf("Invalid Operation Try Again\n");
+    }
+
+
+}
+int update()
+{
+    int op;
+    printf("Select Insert List Operation\n");
+    printf("1. Update\n");
+    printf("Input operation: ");
+    scanf("%d", &op);
+    if(op==1)
+    {
+        printf("Updated\n");
+
+    }
+    else
+    {
+        printf("Invalid Operation Try Again\n");
+    }
+
+
+}
+int search()
+{
+    int op;
+    printf("Select Insert List Operation\n");
+    printf("1. Search\n");
+    printf("Input operation: ");
+    scanf("%d", &op);
+    if(op==1)
+    {
+        printf("Found\n");
+
+    }
+    else
+    {
+        printf("Not Found\n");
+    }
+
+
+}
+int close()
+{
+    int op;
+    printf("Select Insert List Operation\n");
+    printf("Input operation: ");
+    scanf("%d", &op);
+    if(op==1)
+    {
+        printf("File Closed");
+
+    }
+    else
+    {
+        printf("Invalid Operation Try Again\n");
+    }
+
+
+}
+
 
